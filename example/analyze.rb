fd75bffd0af193af2b6b37ed2f33db6c9b004d49
henteko <henteko07@gmail.com>
2016-08-03 03:44:40 +0900

    Add comment

--- example/analyze.rb
+++ example/analyze.rb
@@ -1,5 +1,7 @@
 require 'microsoft_computer_vision'
 
+# See: https://dev.projectoxford.ai/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1fa
+
 subscription_key = ENV['SUBSCRIPTION_KEY']
 image_url = 'https://i.ytimg.com/vi/lJdfoNIxYV0/maxresdefault.jpg'
 
